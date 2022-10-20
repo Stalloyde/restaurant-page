8dd70377c5130618e46a68630d2861b6f3335a09
Stalloyde <stalloyde93@gmail.com>
2022-10-20 14:50:00 +0800

    Hook up texts' initial load from its own modules
    WIP: Image

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import _ from 'lodash';
+import './style.css';
+import {createTitle, createCatchPhrase} from './initial-load.js';
 
-console.log('test')
-console.log('test')
-console.log('test')
-console.log('test')
+
+(function initialLoad () {
+    const content = document.getElementById("content");
+    
+    const title = document.createElement("div");
+    title.className = "title";
+    title.textContent = createTitle("The World's Greatest Kebab");
+    content.appendChild(title);
+    
+    const catchPhrase = document.createElement("div");
+    catchPhrase.className = "catch-phrase";
+    catchPhrase.textContent = createCatchPhrase("Sometimes, the best thing comes from a giant skewer");
+    content.appendChild(catchPhrase);
+})();
