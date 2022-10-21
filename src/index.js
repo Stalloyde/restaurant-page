ffc0be98f1ca78e673cf46cec236108534f55d07
Stalloyde <stalloyde93@gmail.com>
2022-10-21 14:42:48 +0800

    Add modal tab buttons

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import _ from 'lodash';
 import './style.css';
-import {createTitle, createCatchPhrase, loadImage} from './initial-load.js';
+import {createTitle, createCatchPhrase, loadImage, createHomeTab, createMenuTab, createContactTab} from './initial-load.js';
 
 (function initialLoad () {
     createTitle();
     createCatchPhrase();
     loadImage();
+    createHomeTab()
+    createMenuTab()
+    createContactTab()
 })();
+
 
--- src/initial-load.js
+++ src/initial-load.js
@@ -1,4 +1,5 @@
 const content = document.getElementById("content");
+
 export function createTitle () {
     const title = document.createElement("div");
     title.className = "title";
@@ -18,4 +19,29 @@ export function loadImage () {
     image.className = "background-img";
     image.innerHTML = "<img src= /home/stalloyde/repos/restaurant-page/src/kebab.jpg>";
     content.appendChild(image);;
+};
+
+const tabContainer = document.createElement("div");
+tabContainer.className = "tab-container";
+content.appendChild(tabContainer)
+
+export function createHomeTab () {
+    const homeTab = document.createElement("button");
+    homeTab.className = "home-tab";
+    homeTab.textContent = "Home";
+    tabContainer.appendChild(homeTab);
+};
+
+export function createMenuTab () {
+    const menuTab = document.createElement("button");
+    menuTab.className = "menu-tab";
+    menuTab.textContent = "Menu";
+    tabContainer.appendChild(menuTab);
+};
+
+export function createContactTab () {
+    const contactTab = document.createElement("button");
+    contactTab.className = "contact-tab";
+    contactTab.textContent = "Contact";
+    tabContainer.appendChild(contactTab);
 };
