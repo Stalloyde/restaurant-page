fe197b5abf1b16615e215dca2aecd9aa733875bc
Stalloyde <stalloyde93@gmail.com>
2022-10-21 14:08:30 +0800

    Move element create and append to module file

--- src/index.js
+++ src/index.js
@@ -3,20 +3,8 @@ import './style.css';
 import {createTitle, createCatchPhrase, loadImage} from './initial-load.js';
 
 (function initialLoad () {
-    const content = document.getElementById("content");
-    
-    const title = document.createElement("div");
-    title.className = "title";
-    title.textContent = createTitle();
-    content.appendChild(title);
-    
-    const catchPhrase = document.createElement("div");
-    catchPhrase.className = "catch-phrase";
-    catchPhrase.textContent = createCatchPhrase();
-    content.appendChild(catchPhrase);
-
-    const image = document.createElement("div");
-    image.className = "background-img";
-    image.innerHTML = loadImage();
-    content.appendChild(image);
+    createTitle();
+    createCatchPhrase();
+    loadImage();
 })();
+
--- src/initial-load.js
+++ src/initial-load.js
@@ -1,11 +1,21 @@
+const content = document.getElementById("content");
 export function createTitle () {
-    return "The World's Greatest Kebab";
+    const title = document.createElement("div");
+    title.className = "title";
+    title.textContent = "The World's Greatest Kebab";
+    content.appendChild(title);;
 };
     
 export function createCatchPhrase () {
-    return "Sometimes, the best thing comes from a giant skewer";
+    const catchPhrase = document.createElement("div");
+    catchPhrase.className = "catch-phrase";
+    catchPhrase.textContent = "Sometimes, the best things come from a giant skewer";
+    content.appendChild(catchPhrase);
 };
 
 export function loadImage () {
-    return "<img src= /home/stalloyde/repos/restaurant-page/src/kebab.jpg>";
+    const image = document.createElement("div");
+    image.className = "background-img";
+    image.innerHTML = "<img src= /home/stalloyde/repos/restaurant-page/src/kebab.jpg>";
+    content.appendChild(image);;
 };
