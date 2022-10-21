2a6f73c4d13c1b877fdb3a658c7f7d0712753147
Stalloyde <stalloyde93@gmail.com>
2022-10-21 12:39:00 +0800

    Transfer image load from static html to JS.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import _ from 'lodash';
 import './style.css';
-import {createTitle, createCatchPhrase} from './initial-load.js';
-
+import {createTitle, createCatchPhrase, loadImage} from './initial-load.js';
 
 (function initialLoad () {
     const content = document.getElementById("content");
     
     const title = document.createElement("div");
     title.className = "title";
-    title.textContent = createTitle("The World's Greatest Kebab");
+    title.textContent = createTitle();
     content.appendChild(title);
     
     const catchPhrase = document.createElement("div");
     catchPhrase.className = "catch-phrase";
-    catchPhrase.textContent = createCatchPhrase("Sometimes, the best thing comes from a giant skewer");
+    catchPhrase.textContent = createCatchPhrase();
     content.appendChild(catchPhrase);
+
+    const image = document.createElement("div");
+    image.className = "background-img";
+    image.innerHTML = loadImage();
+    content.appendChild(image);
 })();
--- src/initial-load.js
+++ src/initial-load.js
@@ -1,7 +1,11 @@
-export function createTitle (string) {
-    return string;
+export function createTitle () {
+    return "The World's Greatest Kebab";
 };
     
-export function createCatchPhrase (string) {
-    return string;
+export function createCatchPhrase () {
+    return "Sometimes, the best thing comes from a giant skewer";
+};
+
+export function loadImage () {
+    return "<img src= /home/stalloyde/repos/restaurant-page/src/kebab.jpg>";
 };
