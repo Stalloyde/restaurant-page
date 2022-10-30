bef1fb0667c222db89096c332f1a85b90ee1786e
Stalloyde <stalloyde93@gmail.com>
2022-10-30 18:28:38 +0800

    Add & style location, opening hours and phrase

--- src/home-tab.js
+++ src/home-tab.js
@@ -13,11 +13,19 @@ export function homeTabContent () {
     
     const phrase2 = document.createElement("div");
     phrase2.className = "phrase2";
-    phrase2.textContent += `"Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    phrase2.textContent = `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
     Corporis quos voluptas illum tenetur culpa tempore facilis at debitis doloribus, 
     ducimus laudantium praesentium et aut rem ea labore maxime molestiae ipsum repellendus
-     ullam! Similique, ut quo.."`;
+     ullam! Similique, ut quoc`;
     tabContent.appendChild(phrase2);
+
+    const phrase3 = document.createElement("div");
+    phrase3.className = "phrase3";
+    phrase3.textContent = `"Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Corporis quos voluptas illum tenetur culpa tempore facilis at debitis doloribus, 
+    ducimus laudantium praesentium et aut rem ea labore maxime molestiae ipsum repellendus
+     ullam! Similique, ut quoc.."`;
+    tabContent.appendChild(phrase3);
 
     const reserveBtnContainer = document.createElement("div");
     reserveBtnContainer.className = "reserve-btn-container";
@@ -26,7 +34,30 @@ export function homeTabContent () {
     const reserveBtn = document.createElement("button");
     reserveBtn.className = "reserve-btn";
     reserveBtn.textContent = "Make a Reservation";
-    reserveBtnContainer.appendChild(reserveBtn);
+    reserveBtnContainer.appendChild(reserveBtn);   
+
+
+    const infoContainer = document.createElement("div");
+    infoContainer.className = "info-container";
+    tabContent.appendChild(infoContainer);
+    
+    const openingHoursContainer = document.createElement("div");
+    openingHoursContainer.className = "opening-hours-container";
+    infoContainer.appendChild(openingHoursContainer);
+    openingHoursContainer.textContent += "Opening Hours:";         
+    const openingHoursInfo = document.createElement("div");
+    openingHoursInfo.className = "opening-hours-info";
+    openingHoursContainer.appendChild(openingHoursInfo);
+    openingHoursInfo.textContent = "Daily @ 10:00am - 10:00pm";
+
+    const locationContainer = document.createElement("div");
+    locationContainer.className = "location-container";
+    infoContainer.appendChild(locationContainer);
+    locationContainer.textContent += "Location:";
+    const locationInfo = document.createElement("div");
+    locationInfo.className = "location-info";
+    locationContainer.appendChild(locationInfo);
+    locationInfo.textContent = "#01-423, Hue Mall, 46 Turkish Avenue, G88623, Turkey";
 }
 
 // add opening hours & location at home page, social media logos at footer
