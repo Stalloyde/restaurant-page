04e26d848c2feb5e32e4d98f01f35f7aac4d4817
Stalloyde <stalloyde93@gmail.com>
2022-11-04 13:48:25 +0800

    Refactor code

--- src/home-tab.js
+++ src/home-tab.js
@@ -1,63 +1,99 @@
+const title = document.createElement("div");
+const slogan = document.createElement("div");
+const phrase1 = document.createElement("div");
+const phrase2 = document.createElement("div");
+const reserveBtnContainer = document.createElement("div");
+const reserveBtn = document.createElement("button");
+const infoContainer = document.createElement("div");
+const openingHoursContainer = document.createElement("div");
+const openingHoursInfo = document.createElement("div");
+const locationContainer = document.createElement("div");
+const locationInfo = document.createElement("div");
+
 const tabContent = document.querySelector(".tab-content");
 
-export function homeTabContent () {
+    function createTitle () {
+        title.className = "title";
+        title.textContent = "Baba Grill House";
+        tabContent.appendChild(title);
+    };
+
+    function createSlogan () {
+        slogan.className = "slogan";
+        slogan.textContent = "The best Kebabs in the World!";
+        tabContent.appendChild(slogan);
+    };
+
+    function createPhrase1 () {
+        phrase1.className = "phrase1";
+        phrase1.textContent = `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Corporis quos voluptas illum tenetur culpa tempore facilis at debitis doloribus, 
+        ducimus laudantium praesentium et aut rem ea labore maxime molestiae ipsum repellendus
+         ullam! Similique, ut quoc`;
+        tabContent.appendChild(phrase1);
+    };
+
+    function createPhrase2 () {
+        phrase2.className = "phrase2";
+        phrase2.textContent = `"Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Corporis quos voluptas illum tenetur culpa tempore facilis at debitis doloribus, 
+        ducimus laudantium praesentium et aut rem ea labore maxime molestiae ipsum repellendus
+         ullam! Similique, ut quoc.."`;
+        tabContent.appendChild(phrase2);
+    };
+
+    function createReserveBtnContainer () { 
+        reserveBtnContainer.className = "reserve-btn-container";
+        tabContent.appendChild(reserveBtnContainer);
+    };
+
+    function createReserveBtn () { 
+        reserveBtn.className = "reserve-btn";
+        reserveBtn.id= "loadreservepage-btn";
+        reserveBtn.textContent = "Make Reservation";
+        reserveBtnContainer.appendChild(reserveBtn);   
+    };
+
+    function createInfoContainer () { 
+        infoContainer.className = "info-container";
+        tabContent.appendChild(infoContainer);
+    };
+
+    function createOpeningHoursContainer () { 
+        openingHoursContainer.className = "opening-hours-container";
+        infoContainer.appendChild(openingHoursContainer);
+        openingHoursContainer.textContent += "Opening Hours:";         
+    };
+
+    function createOpeningHoursInfo () {
+       openingHoursInfo.className = "opening-hours-info";
+       openingHoursContainer.appendChild(openingHoursInfo);
+       openingHoursInfo.textContent = "Daily @ 10:00am - 10:00pm";
+    };
+
+    function createLocationContainer () { 
+        locationContainer.className = "location-container";
+        infoContainer.appendChild(locationContainer);
+        locationContainer.textContent += "Location:";
+    };
     
-    const title = document.createElement("div");
-    title.className = "title";
-    title.textContent = "Baba Grill House";
-    tabContent.appendChild(title);
+    function createLocationInfo () {
+        locationInfo.className = "location-info";
+        locationContainer.appendChild(locationInfo);
+        locationInfo.textContent = "#01-423, Hue Mall, 46 Turkish Avenue, G88623, Turkey";
+    };
 
-    const slogan = document.createElement("div");
-    slogan.className = "slogan";
-    slogan.textContent = "The best Kebabs in the World!";
-    tabContent.appendChild(slogan);
-    
-    const phrase1 = document.createElement("div");
-    phrase1.className = "phrase1";
-    phrase1.textContent = `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-    Corporis quos voluptas illum tenetur culpa tempore facilis at debitis doloribus, 
-    ducimus laudantium praesentium et aut rem ea labore maxime molestiae ipsum repellendus
-     ullam! Similique, ut quoc`;
-    tabContent.appendChild(phrase1);
-
-    const phrase2 = document.createElement("div");
-    phrase2.className = "phrase2";
-    phrase2.textContent = `"Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-    Corporis quos voluptas illum tenetur culpa tempore facilis at debitis doloribus, 
-    ducimus laudantium praesentium et aut rem ea labore maxime molestiae ipsum repellendus
-     ullam! Similique, ut quoc.."`;
-    tabContent.appendChild(phrase2);
-
-    const reserveBtnContainer = document.createElement("div");
-    reserveBtnContainer.className = "reserve-btn-container";
-    tabContent.appendChild(reserveBtnContainer);
-    
-    const reserveBtn = document.createElement("button");
-    reserveBtn.className = "reserve-btn";
-    reserveBtn.id= "loadreservepage-btn";
-    reserveBtn.textContent = "Make Reservation";
-    reserveBtnContainer.appendChild(reserveBtn);   
-
-    const infoContainer = document.createElement("div");
-    infoContainer.className = "info-container";
-    tabContent.appendChild(infoContainer);
-    
-    const openingHoursContainer = document.createElement("div");
-    openingHoursContainer.className = "opening-hours-container";
-    infoContainer.appendChild(openingHoursContainer);
-    openingHoursContainer.textContent += "Opening Hours:";         
-    const openingHoursInfo = document.createElement("div");
-    openingHoursInfo.className = "opening-hours-info";
-    openingHoursContainer.appendChild(openingHoursInfo);
-    openingHoursInfo.textContent = "Daily @ 10:00am - 10:00pm";
-
-    const locationContainer = document.createElement("div");
-    locationContainer.className = "location-container";
-    infoContainer.appendChild(locationContainer);
-    locationContainer.textContent += "Location:";
-    const locationInfo = document.createElement("div");
-    locationInfo.className = "location-info";
-    locationContainer.appendChild(locationInfo);
-    locationInfo.textContent = "#01-423, Hue Mall, 46 Turkish Avenue, G88623, Turkey";
-
-};
+    export function homeTabContent () {
+        createTitle();
+        createSlogan();
+        createPhrase1();
+        createPhrase2();
+        createReserveBtnContainer();
+        createReserveBtn();
+        createInfoContainer();
+        createOpeningHoursContainer();
+        createOpeningHoursInfo();
+        createLocationContainer();
+        createLocationInfo();
+    };
+        
--- src/reserve-tab.js
+++ src/reserve-tab.js
@@ -1,42 +1,70 @@
 const tabContent = document.querySelector(".tab-content");
+const title = document.createElement("div");
+const form = document.createElement("form");
+const nameContainer = document.createElement("div");
+const nameLabel = document.createElement("label");
+const nameInput = document.createElement("input");
+const telContainer = document.createElement("div");
+const tel = document.createElement("label");
+const telInput = document.createElement("input");
+const emailContainer = document.createElement("div");
+const email = document.createElement("label");
+const emailInput = document.createElement("input");
+const paxContainer = document.createElement("div");
+const pax = document.createElement("label");
+const paxInput = document.createElement("input");
+const requestContainer = document.createElement("div");
+const requestInput = document.createElement("input");
+const dateContainer = document.createElement("div");
+const dateInput = document.createElement("input");
+const timeContainer = document.createElement("div");
+const timeInput = document.createElement("input");
+const submitContainer = document.createElement("div");
+const submit = document.createElement("input");
 
-export function reserveTabContent ()  {
-    const title = document.createElement("div");
+function createTitle ()  {
     title.className = "title";
     title.textContent = "Baba Grill House";
     tabContent.appendChild(title);
-    
-    const form = document.createElement("form");
+};
+
+function createForm ()  {
     form.setAttribute("method", "post");
     tabContent.appendChild(form);
-    
-    const nameContainer = document.createElement("div");
+};
+
+function createNameContainer ()  {
     nameContainer.className = "name-container"
     form.appendChild(nameContainer);
+};
 
-    const nameLabel = document.createElement("label");
+function createNameLabel ()  {
     nameLabel.setAttribute("for","user-name");
     nameLabel.textContent = "Name: ";
     nameContainer.appendChild(nameLabel);
+};
 
-    const nameInput = document.createElement("input");
+function createNameInput ()  {
     nameInput.setAttribute("id","user-name");
     nameInput.setAttribute("name","user-name");
     nameInput.setAttribute("type","text");
     nameInput.setAttribute("pattern","^[A-Za-z]+$");
     nameInput.setAttribute("required","");
     nameContainer.appendChild(nameInput);
+};
 
-    const telContainer = document.createElement("div");
+function createTelContainer ()  {
     telContainer.className = "tel-container"
     form.appendChild(telContainer);
+};
 
-    const tel = document.createElement("label");
+function createTelLabel()  {
     tel.setAttribute("for","user-tel");
     tel.textContent = "Contact Number: ";
     telContainer.appendChild(tel);
+};
 
-    const telInput = document.createElement("input");
+function createTelInput ()  {
     telInput.setAttribute("id","user-tel");
     telInput.setAttribute("name","user-tel");
     telInput.setAttribute("type","tel");
@@ -44,33 +72,39 @@ export function reserveTabContent ()  {
     telInput.setAttribute("required","");
     telInput.setAttribute("title","Only numerics are allowed eg: 222552245");
     telContainer.appendChild(telInput);
+};
 
-    const emailContainer = document.createElement("div");
+function createEmailContainer ()  {
     emailContainer.className = "email-container"
     form.appendChild(emailContainer);
+};
 
-    const email = document.createElement("label");
+function createEmailLabel ()  {
     email.setAttribute("for","user-email");
     email.textContent = "E-mail: ";
     emailContainer.appendChild(email);
+};
 
-    const emailInput = document.createElement("input");
+function createEmailInput()  {
     emailInput.setAttribute("id","user-email");
     emailInput.setAttribute("name","user-email");
     emailInput.setAttribute("type","email");
     emailInput.setAttribute("required","");
     emailContainer.appendChild(emailInput);
+};
 
-    const paxContainer = document.createElement("div");
+function createPaxContainer ()  {
     paxContainer.className = "pax-container"
     form.appendChild(paxContainer);
+};
 
-    const pax = document.createElement("label");
+function createPaxLabel ()  {
     pax.setAttribute("for","user-pax");
     pax.textContent = "No. of Pax: ";
     paxContainer.appendChild(pax);
+};
 
-    const paxInput = document.createElement("input");
+function createPaxInput ()  {
     paxInput.setAttribute("id","user-pax");
     paxInput.setAttribute("name","user-pax");
     paxInput.setAttribute("type","number");
@@ -78,31 +112,35 @@ export function reserveTabContent ()  {
     paxInput.setAttribute("min","1");
     paxInput.setAttribute("max","10");
     paxContainer.appendChild(paxInput);
+};
 
-    const requestContainer = document.createElement("div");
+function createRequestContainer ()  {
     requestContainer.className = "request-container"
     form.appendChild(requestContainer);
     const request = document.createElement("label");
     request.setAttribute("for","user-request");
     request.textContent = "Special Requests (We'll do our best to accomodate): ";
     requestContainer.appendChild(request);
+};
 
-    const requestInput = document.createElement("input");
+function createRequestInput ()  {
     requestInput.setAttribute("id","user-request");
     requestInput.setAttribute("name","user-request");
     requestInput.setAttribute("type","text");
     requestInput.setAttribute("placeholder","eg: 1 piece childseat");
     requestContainer.appendChild(requestInput);
+};
 
-    const dateContainer = document.createElement("div");
+function createDateContainer ()  {
     dateContainer.className = "date-container"
     form.appendChild(dateContainer);
     const date = document.createElement("label");
     date.setAttribute("for","user-date");
     date.textContent = "Date of Reservation: ";
     dateContainer.appendChild(date);
+};
 
-    const dateInput = document.createElement("input");
+function createDateInput ()  {
     dateInput.setAttribute("id","user-date");
     dateInput.setAttribute("name","user-date");
     const minDate = new Date();
@@ -115,16 +153,18 @@ export function reserveTabContent ()  {
     dateInput.setAttribute("min", minDate);
     dateInput.setAttribute("max", getNextMonth());
     dateContainer.appendChild(dateInput);
+};
 
-    const timeContainer = document.createElement("div");
+function createTimeContainer ()  {
     timeContainer.className = "time-container"
     form.appendChild(timeContainer);
     const time = document.createElement("label");
     time.setAttribute("for","user-time");
     time.textContent = "Time of Reservation (10:00 - 22:00): ";
     timeContainer.appendChild(time);
+};
 
-    const timeInput = document.createElement("input");
+function createTimeInput ()  {
     timeInput.setAttribute("id","user-time");
     timeInput.setAttribute("name","user-time");
     timeInput.setAttribute("type","time");
@@ -132,16 +172,43 @@ export function reserveTabContent ()  {
     timeInput.setAttribute("max","22:00");
     timeInput.setAttribute("value","10:00");
     timeContainer.appendChild(timeInput);
+};
 
-    const submitContainer = document.createElement("div");
+function createSubmitContainer ()  {
     submitContainer.className = "submit-container";
     form.appendChild(submitContainer);
+};
 
-    const submit = document.createElement("input");
+function createSubmitInput ()  {
     submit.setAttribute("type", "submit");
     submit.setAttribute("id", "submit-button");
     submit.textContent = "Submit"
     submit.className = "reserve-btn";
     submitContainer.appendChild(submit);   
     submit.addEventListener("click", function () {confirm("Confirm reservation details?")});
+};
+
+export function reserveTabContent ()  {
+    createTitle();
+    createForm();
+    createNameContainer();
+    createNameLabel();
+    createNameInput();
+    createTelContainer();
+    createTelLabel();
+    createTelInput();
+    createEmailContainer();
+    createEmailLabel();
+    createEmailInput();
+    createPaxContainer();
+    createPaxLabel();
+    createPaxInput();
+    createRequestContainer();
+    createRequestInput();
+    createDateContainer();
+    createDateInput();
+    createTimeContainer();
+    createTimeInput();
+    createSubmitContainer();
+    createSubmitInput();
 }
